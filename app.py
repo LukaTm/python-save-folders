26cfdea21c7c74dc51f2e027a508ee17d5607552
LukaTm <119138345+LukaTm@users.noreply.github.com>
2023-04-24 01:21:08 +0300

    Replace if exists

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ source_directories = [
     os.path.join(os.environ['LOCALAPPDATA'], '..', 'LocalLow'),
     # %APPDATA% - AppData/Roaming
     os.path.join(os.environ['APPDATA']),
+    # Home Directory - C:\Users\username
     os.path.join(os.path.expanduser('~'), 'Documents'),
 ]
 
@@ -37,7 +38,7 @@ with open(file_path, 'r') as f:
             folder_path = os.path.join(directory, folder_name)
             if os.path.exists(folder_path):
                 # Copy the folder to the destination directory
-                shutil.copytree(folder_path, os.path.join(destination_directory, folder_name))
+                shutil.copytree(folder_path, os.path.join(destination_directory, folder_name), dirs_exist_ok=True)
                 found_folder = True
                 break
         if not found_folder:
